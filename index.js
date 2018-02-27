b74372f3bbf5b29fa37eb5f1099acbd180dbc739
EXtremeExploit <gdextremeexploit@gmail.com>
2018-02-27 18:51:19 -0300

    💩
    ADDED: TypeScript for index.js
    ADDED: Dependencies on README.md
    ADDED: test/ to .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const snekfetch = require('snekfetch');
 const API = 'https://discordbots.org/api/';
+const { Bot, Bots, BotsQuery, Stats, User, UserSocial, Votes } = require('./ts/index');
+const { Client, Shard } = require('discord.js')
 
 class DBLAPI {
+  /**
+   * 
+   * @param {string} token 
+   * @param {Client} client 
+   */
   constructor(token, client) {
     this.token = token;
     if (client) {
@@ -14,7 +21,13 @@ class DBLAPI {
       });
     }
   }
-
+  /**
+   * 
+   * @param {string} method 
+   * @param {string} endpoint 
+   * @param {any} data 
+   * @param {boolean} auth 
+   */
   _request(method, endpoint, data, auth) {
     const request = snekfetch[method](API + endpoint);
     if (method === 'post' && data) request.send(data);
@@ -22,7 +35,12 @@ class DBLAPI {
     if (auth) request.set({ Authorization: this.token });
     return request;
   }
-
+  /**
+   * 
+   * @param {number} serverCount 
+   * @param {number} shardId 
+   * @param {number} shardCount 
+   */
   async postStats(serverCount, shardId, shardCount) {
     if (!this.token) throw new Error('This function requires a token to be set');
     if (!serverCount && !this.client) throw new Error('postStats requires 1 argument');
@@ -41,39 +59,63 @@ class DBLAPI {
     const response = await this._request('post', 'bots/stats', data, true);
     return response.body;
   }
-
+  /**
+   * 
+   * @param {number} id
+   * @returns {Promise<Stats>}
+   */
   async getStats(id) {
     if (!id && !this.client) throw new Error('getStats requires id as argument');
     if (!id) id = this.client.user.id;
     const response = await this._request('get', `bots/${id}/stats`);
     return response.body;
   }
-
+  /**
+   * 
+   * @param {number} id 
+   * @returns {Promise<Bot>}
+   */
   async getBot(id) {
     if (!id && !this.client) throw new Error('getBot requires id as argument');
     if (!id) id = this.client.user.id;
     const response = await this._request('get', `bots/${id}`);
     return response.body;
   }
-
+  /**
+   * 
+   * @param {number} id 
+   * @returns {Promise<User>}
+   */
   async getUser(id) {
     if (!id) throw new Error('getUser requires id as argument');
     const response = await this._request('get', `users/${id}`);
     return response.body;
   }
-
+  /**
+   * 
+   * @param {BotsQuery} query 
+   * @returns {Promise<Bots>}
+   */
   async getBots(query) {
     const response = await this._request('get', 'bots', query);
     return response.body;
   }
-
+  /**
+   * 
+   * @param {boolean} onlyids 
+   * @param {number} days
+   * @returns {Promise<Votes[]>}
+   */
   async getVotes(onlyids, days) {
     if (!this.token) throw new Error('This function requires a token to be set');
     if (days < 0 || days > 31) throw new Error('Days parameter out of bounds (0-31)');
     const response = await this._request('get', 'bots/votes', { onlyids, days }, true);
     return response.body;
   }
-
+  /**
+   * @param {number} id 
+   * @returns {Promise<boolean>}
+   */
   async hasVoted(id) {
     if (!this.token) throw new Error('This function requires a token to be set');
     const voters = await this.getVotes(true);
---
+++ ts/index.d.ts
@@ -0,0 +1,76 @@
+import { Client, Shard } from "discord.js";
+
+export declare class Stats {
+    server_count: number;
+    shards: Shard[];
+}
+
+export declare class Bot {
+    id: number;
+    username: string;
+    discriminator: string;
+    avatar?: string;
+    defAvatar: string;
+    lib: string;
+    prefix: string;
+    shortdesc: string;
+    longdesc?: string;
+    tags: string[];
+    website?: string;
+    support?: string;
+    github?: string;
+    owners: number[];
+    invite?: string;
+    date: Date;
+    certifiedBot: boolean;
+    vanity?: string;
+    points: number;
+}
+
+export declare class User {
+    id: number;
+    username: string;
+    discriminator: string;
+    avatar?: string;
+    defAvatar: string;
+    bio?: string;
+    banner?: string;
+    social: UserSocial;
+    color?: string;
+    supporter: boolean;
+    certifiedDev: boolean;
+    mod: boolean;
+    webMod; boolean;
+    admin: boolean;
+}
+
+export declare class UserSocial {
+    youtube?: string;
+    reddit?: string;
+    twitter?: string;
+    instagram?: string;
+    github?: string;
+}
+
+export declare class BotsQuery {
+    limit?: number;
+    offset?: number;
+    search: string;
+    sort: string;
+    fields?: string;
+}
+
+export declare class Bots {
+    results: Bot[];
+    limit: number;
+    offset: number;
+    count: number;
+    total: number;
+}
+
+export declare class Votes {
+    username: string;
+    discriminator: string;
+    id: string;
+    avatar: string;
+}
