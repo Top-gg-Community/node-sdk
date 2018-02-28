d26201b37e22d1a2f5482fd4a4027e0f7924016e
Yukine <blazylp@gmail.com>
2018-02-28 18:10:51 +0200

    added JSDocs and Typescript support (#3)
    * added JSDocs and Typescript support
    
    * small adjustments
    
    * small fix
    
    * add correct docs
    
    * change typings for _request

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const snekfetch = require('snekfetch');
 const API = 'https://discordbots.org/api/';
 
 class DBLAPI {
+  /**
+   * Creates a new DBLAPI Instance.
+   * @param {string} token Your Discordbots token for this bot.
+   * @param {any} [client] Your Client Instance, if present it will auto update your stats every 30 minutes.
+   */
   constructor(token, client) {
     this.token = token;
     if (client) {
@@ -14,7 +19,15 @@ class DBLAPI {
       });
     }
   }
-
+  /**
+   * Creates the request with snekfetch.
+   * @param {string} method Http method to use.
+   * @param {string} endpoint API Endpoint to use.
+   * @param {Object} [data] Data to send with the request.
+   * @param {boolean} [auth] Boolean indicating if auth is needed.
+   * @private
+   * @returns {Promise<snekfetch>}
+   */
   _request(method, endpoint, data, auth) {
     const request = snekfetch[method](API + endpoint);
     if (method === 'post' && data) request.send(data);
@@ -22,7 +35,13 @@ class DBLAPI {
     if (auth) request.set({ Authorization: this.token });
     return request;
   }
-
+  /**
+   * Post Stats to Discordbotlist.
+   * @param {number|number[]} serverCount The Server count of your bot.
+   * @param {number} [shardId] The ID of this shard.
+   * @param {number} [shardCount] The Count of all Shards of your bot.
+   * @returns {Promise<Object>}
+   */
   async postStats(serverCount, shardId, shardCount) {
     if (!this.token) throw new Error('This function requires a token to be set');
     if (!serverCount && !this.client) throw new Error('postStats requires 1 argument');
@@ -41,39 +60,64 @@ class DBLAPI {
     const response = await this._request('post', 'bots/stats', data, true);
     return response.body;
   }
-
+  /**
+   * Gets Stats from a bot.
+   * @param {string} id The ID of the bot you want to get the stats from.
+   * @returns {Promise<Buffer>}
+   */
   async getStats(id) {
     if (!id && !this.client) throw new Error('getStats requires id as argument');
     if (!id) id = this.client.user.id;
     const response = await this._request('get', `bots/${id}/stats`);
     return response.body;
   }
-
+  /**
+   * Gets Information about a bot.
+   * @param {string} id The ID of the bot you want to get the Information from.
+   * @returns {Promise<Buffer>}
+   */
   async getBot(id) {
     if (!id && !this.client) throw new Error('getBot requires id as argument');
     if (!id) id = this.client.user.id;
     const response = await this._request('get', `bots/${id}`);
     return response.body;
   }
-
+  /**
+   * Gets Information about a User.
+   * @param {string} id The ID of the User you want to get the Information from.
+   * @returns {Promise<Buffer>}
+   */
   async getUser(id) {
     if (!id) throw new Error('getUser requires id as argument');
     const response = await this._request('get', `users/${id}`);
     return response.body;
   }
-
+  /**
+   * Gets a list of bots matching your query.
+   * @param {Object} query The Query for the search.
+   * @returns {Promise<Buffer>}
+   */
   async getBots(query) {
     const response = await this._request('get', 'bots', query);
     return response.body;
   }
-
+  /**
+   * Gets Votes from your bot.
+   * @param {boolean} onlyids boolean indicating if this request should only return IDs.
+   * @param {number} days a number indicating how much days ago the votes should be shown.
+   * @returns {Promise<Buffer>}
+   */
   async getVotes(onlyids, days) {
     if (!this.token) throw new Error('This function requires a token to be set');
     if (days < 0 || days > 31) throw new Error('Days parameter out of bounds (0-31)');
     const response = await this._request('get', 'bots/votes', { onlyids, days }, true);
     return response.body;
   }
-
+  /**
+   * Returns if an User has voted for your Bot.
+   * @param {string} id The ID of the User to check for.
+   * @returns {Promise<boolean>}
+   */
   async hasVoted(id) {
     if (!this.token) throw new Error('This function requires a token to be set');
     const voters = await this.getVotes(true);
---
+++ typings/index.d.ts
@@ -0,0 +1,93 @@
+declare module "dblapi.js" {
+	export class DBLAPI {
+		constructor(token: string, client?: any)
+		
+		public postStats(serverCount: number, shardId?: number, shardCount?: number): Promise<Buffer>
+		public getStats(id: string): Promise<BotStats>
+		public getBot(id: string): Promise<Bot>
+		public getUser(id: string): Promise<User>
+		public getBots(query: BotsQuery): Promise<BotSearchResult>
+		public getVotes(onlyids?: boolean, days?: number): Promise<Votes[]>
+		public hasVoted(id: string): Promise<boolean>
+
+		public token?: string;
+	
+		private _request(method: string, endpoint: string, data?: Object, auth?: boolean): Promise<Object>
+	}
+
+	type BotStats = {
+		server_count: number;
+		shards: number[];
+		shard_count: number;
+	}
+		
+	type Bot = {
+		id: number;
+		username: string;
+		discriminator: string;
+		avatar?: string;
+		defAvatar: string;
+		lib: string;
+		prefix: string;
+		shortdesc: string;
+		longdesc?: string;
+		tags: string[];
+		website?: string;
+		support?: string;
+		github?: string;
+		owners: number[];
+		invite?: string;
+		date: Date;
+		certifiedBot: boolean;
+		vanity?: string;
+		points: number;
+	}
+		
+	type User = {
+		id: number;
+		username: string;
+		discriminator: string;
+		avatar?: string;
+		defAvatar: string;
+		bio?: string;
+		banner?: string;
+		social: UserSocial;
+		color?: string;
+		supporter: boolean;
+		certifiedDev: boolean;
+		mod: boolean;
+		webMod: boolean;
+		admin: boolean;
+	}
+		
+	type UserSocial = {
+		youtube?: string;
+		reddit?: string;
+		twitter?: string;
+		instagram?: string;
+		github?: string;
+	}
+		
+	type BotsQuery ={
+		limit?: number;
+		offset?: number;
+		search: string;
+		sort: string;
+		fields?: string;
+	}
+		
+	type BotSearchResult = {
+		results: Bot[];
+		limit: number;
+		offset: number;
+		count: number;
+		total: number;
+	}
+		
+	type Votes = {
+		username: string;
+		discriminator: string;
+		id: string;
+		avatar: string;
+	}
+}
