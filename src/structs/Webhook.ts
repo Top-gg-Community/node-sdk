590d0ad0e42c6bf3eda2599220b2a8ffaa27d52a
Julian Berryessa <jb@jt3ch.net>
2021-04-13 19:45:39 -0400

    Add new Webhook.listener() method (#55)

--- src/structs/Webhook.ts
+++ src/structs/Webhook.ts
@@ -1,7 +1,17 @@
 import getBody from 'raw-body'
 import qs from 'querystring'
 
+import { Request, Response, NextFunction } from 'express'
+
 import { WebhookPayload } from '../typings'
+
+export interface WebhookOptions {
+  /**
+   * Handles an error created by the function passed to Webhook.listener()
+   * @default console.error
+   */
+  error?: (error: Error) => void | Promise<void>
+}
 
 /**
  * Top.gg Webhook
@@ -12,10 +22,10 @@ import { WebhookPayload } from '../typings'
  * const app = express()
  * const wh = new Webhook('webhookauth123')
  * 
- * app.post('/dblwebhook', wh.middleware(), (req, res) => {
- *   // req.vote is your vote object e.g
- *   console.log(req.vote.user) // => 321714991050784770
- * })
+ * app.post('/dblwebhook', wh.listener(vote => {
+ *   // vote is your vote object e.g
+ *   console.log(vote.user) // => 321714991050784770
+ * }))
  * 
  * app.listen(80)
  * 
@@ -24,18 +34,24 @@ import { WebhookPayload } from '../typings'
  * // Authorization: webhookauth123
  */
 export class Webhook {
+  public options: WebhookOptions
+
   /**
    * Create a new webhook client instance
    * @param authorization Webhook authorization to verify requests
    */
-  constructor (private authorization?: string) {}
+  constructor (private authorization?: string, options: WebhookOptions = {}) {
+    this.options = {
+      error: options.error ?? console.error
+    }
+  }
 
   private _formatIncoming (body): WebhookPayload {
     if (body?.query?.length > 0) body.query = qs.parse(body.query.substr(1))
     return body
   }
 
-  private _parseRequest (req, res): Promise<WebhookPayload|false> {
+  private _parseRequest (req: Request, res: Response): Promise<WebhookPayload|false> {
     return new Promise(resolve => {
       if (this.authorization && req.headers.authorization !== this.authorization) return res.status(403).json({ error: 'Unauthorized' })
       // parse json
@@ -57,7 +73,42 @@ export class Webhook {
   }
 
   /**
-   * Middleware function to pass to express, sets req.vote to the payload
+   * Listening function for handling webhook requests
+   * @example
+   * app.post('/webhook', wh.listener((vote) => {
+   *   console.log(vote.user) // => 395526710101278721
+   * }))
+   * @param fn Vote handling function, this function can also throw an error to allow for the webhook to resend from Top.gg
+   * @example
+   * // Throwing an error to resend the webhook
+   * app.post('/webhook/', wh.listener((vote) => {
+   *   // for example, if your bot is offline, you should probably not handle votes and try again
+   *   if (bot.offline) throw new Error('Bot offline')
+   * }))
+   * @returns An express request handler
+   */
+  public listener (fn: (payload: WebhookPayload, req?: Request, res?: Response, next?: NextFunction) => void | Promise<void>) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      const response = await this._parseRequest(req, res)
+      if (!response) return
+
+      try {
+        await fn(response, req, res, next)
+
+        if (!res.headersSent) {
+          res.sendStatus(204)
+        }
+      } catch (err) {
+        this.options.error(err)
+
+        res.sendStatus(500)
+      }
+    }
+  }
+
+  /**
+   * (Use the new .listener() function) Middleware function to pass to express, sets req.vote to the payload
+   * @deprecated
    * @example
    * app.post('/dblwebhook', wh.middleware(), (req, res) => {
    *   // req.vote is your payload e.g
