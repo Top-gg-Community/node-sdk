3bfb3206f684f39c886b33c6d9ac506617d19f68
moyshik7 <moyshik7@gmail.com>
2021-05-19 11:30:13 +0600

    Added post() in autopost to instantly post status

--- src/structs/Autopost.ts
+++ src/structs/Autopost.ts
@@ -20,8 +20,13 @@ export class Autopost {
         this._interval = intervalTime;
     }
     public init(): void {
+        /*
+        Posts status every x time (x = _interval)
+        Good for using in "ready" event
+        Might use in ground level before Client logging in (Won't work then)
+        */
         setInterval(() => {
-            if(!this._client.options.shardCount || this._client.options.shardCount <= 1){ //Not sharding on only one shard
+            if(!this._client.options.shardCount || (this._client.options.shardCount <= 1)){ //Not sharding or only one shard
                 this._api.postStats({ //Post the status using _api
                     serverCount: this._client.guilds.cache.size //Guild count of the bot
                 })
@@ -34,12 +39,30 @@ export class Autopost {
             }
         }, this._interval); //Do this every _interval ms
     }
+    public post(): void {
+        /*
+        Posts status instantly
+        Good for using in "guildCreate" and "guildDelete" events
+        */
+        if(!this._client.options.shardCount || (this._client.options.shardCount <= 1)){ //Not sharding or only one shard
+            this._api.postStats({ //Post the status using _api
+                serverCount: this._client.guilds.cache.size //Guild count of the bot
+            })
+        } else {
+            this._api.postStats({ //Post the status using _api
+                serverCount: this._client.guilds.cache.size, //Guild count of the bot
+                shardId: this._client.shard.ids[0], //The shard id the _client is from
+                shardCount: this._client.options.shardCount //The shard count
+            })
+        }
+    }
 }
 
 
 
 /*
-Example:
+Example (autopost every x second):
+
 import { Client } from "discord.js"
 import { Autopost } from "@top-gg/sdk"
 
@@ -50,6 +73,26 @@ const Autoposter = new Autopost("TOP.GG API TOKEN", Bot, (20*60*1000))
 Bot.once("ready", () => {
     Autoposter.init()
     console.log("Bot is online")
+})
+Bot.login("DISCORD_BOT_TOKEN")
+
+
+Example (post when needed):
+
+import { Client } from "discord.js"
+import { Autopost } from "@top-gg/sdk"
+
+const Bot = new Client()
+
+const Autoposter = new Autopost("TOP.GG API TOKEN", Bot, (20*60*1000))
+
+Bot.once("guildCreate", () => {
+    Autoposter.post();
+    console.log("Added to one more server");
+})
+Bot.once("guildDelete", () => {
+    Autoposter.post();
+    console.log("Removed from one server");
 })
 Bot.login("DISCORD_BOT_TOKEN")
 */
