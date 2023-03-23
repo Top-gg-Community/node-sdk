cc14fb800b69fdd69a7c079d2167ba91923db1ba
Mohammed Keyvanzadeh <mohammadkeyvanzade94@gmail.com>
2023-03-23 06:36:11 +0330

    deps,actions: update deps and make tweaks (#75)
    * deps,actions: update deps and make tweaks
    
    - Update dependencies and developer dependencies to their latest
    versions.
    - Switch from Node-Fetch to Undici.
    - Update the GitHub Actions workflows to their latest versions.
    - Remove Node.js v12 from the testing matrix as it reached it's EoL
    (End-of-Life) stage, and add Node.js v16 and v18 to the testing
    matrix.
    - Align all examples (in README and JSDocs) to the code style of the
    codebase.
    - Inline JSDocs that only have a description.
    - Format the `typedoc.json` file and remove the usage of the `minimal`
    theme as it was removed.
    - Move the `APIOptions` interface to the `src/typings.ts` file and
    export it so it's shown in the documentation.
    - Bump version to 3.1.4.
    
    ---------
    
    Co-authored-by: Julian Berryessa <jb@bit.kiwi>

--- src/structs/Webhook.ts
+++ src/structs/Webhook.ts
@@ -5,6 +5,7 @@ import { WebhookPayload } from "../typings";
 export interface WebhookOptions {
   /**
    * Handles an error created by the function passed to Webhook.listener()
+   *
    * @default console.error
    */
   error?: (error: Error) => void | Promise<void>;
@@ -12,25 +13,27 @@ export interface WebhookOptions {
 
 /**
  * Top.gg Webhook
+ *
  * @example
  * ```js
- * const express = require('express')
- * const { Webhook } = require(`@top-gg/sdk`)
+ * const express = require("express");
+ * const { Webhook } = require("@top-gg/sdk");
  *
- * const app = express()
- * const wh = new Webhook('webhookauth123')
+ * const app = express();
+ * const wh = new Webhook("webhookauth123");
  *
- * app.post('/dblwebhook', wh.listener(vote => {
+ * app.post("/dblwebhook", wh.listener((vote) => {
  *   // vote is your vote object e.g
- *   console.log(vote.user) // => 321714991050784770
- * }))
+ *   console.log(vote.user); // => 321714991050784770
+ * }));
  *
- * app.listen(80)
+ * app.listen(80);
  *
  * // In this situation, your TopGG Webhook dashboard should look like
  * // URL = http://your.server.ip:80/dblwebhook
  * // Authorization: webhookauth123
  * ```
+ *
  * @link {@link https://docs.top.gg/resources/webhooks/#schema | Webhook Data Schema}
  * @link {@link https://docs.top.gg/resources/webhoooks | Webhook Documentation}
  */
@@ -39,6 +42,7 @@ export class Webhook {
 
   /**
    * Create a new webhook client instance
+   *
    * @param authorization Webhook authorization to verify requests
    */
   constructor(private authorization?: string, options: WebhookOptions = {}) {
@@ -86,21 +90,25 @@ export class Webhook {
 
   /**
    * Listening function for handling webhook requests
+   *
    * @example
    * ```js
-   * app.post('/webhook', wh.listener((vote) => {
-   *   console.log(vote.user) // => 395526710101278721
-   * }))
+   * app.post("/webhook", wh.listener((vote) => {
+   *   console.log(vote.user); // => 395526710101278721
+   * }));
    * ```
-   * @param fn Vote handling function, this function can also throw an error to allow for the webhook to resend from Top.gg
+   *
    * @example
    * ```js
    * // Throwing an error to resend the webhook
-   * app.post('/webhook/', wh.listener((vote) => {
+   * app.post("/webhook/", wh.listener((vote) => {
    *   // for example, if your bot is offline, you should probably not handle votes and try again
-   *   if (bot.offline) throw new Error('Bot offline')
-   * }))
+   *   if (bot.offline) throw new Error('Bot offline');
+   * }));
    * ```
+   *
+   * @param fn Vote handling function, this function can also throw an error to
+   *   allow for the webhook to resend from Top.gg
    * @returns An express request handler
    */
   public listener(
@@ -135,13 +143,14 @@ export class Webhook {
 
   /**
    * Middleware function to pass to express, sets req.vote to the payload
+   *
    * @deprecated Use the new {@link Webhook.listener | .listener()} function
    * @example
    * ```js
-   * app.post('/dblwebhook', wh.middleware(), (req, res) => {
+   * app.post("/dblwebhook", wh.middleware(), (req, res) => {
    *   // req.vote is your payload e.g
-   *   console.log(req.vote.user) // => 395526710101278721
-   * })
+   *   console.log(req.vote.user); // => 395526710101278721
+   * });
    * ```
    */
   public middleware() {
@@ -153,8 +162,6 @@ export class Webhook {
       const response = await this._parseRequest(req, res);
       if (!response) return;
       res.sendStatus(204);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore deprecated unsafe assignment
       req.vote = response;
       next();
     };
--- src/typings.ts
+++ src/typings.ts
@@ -1,296 +1,179 @@
-/**
- * Discord ID
- */
+export interface APIOptions {
+  /** Top.gg token */
+  token?: string;
+}
+
+/** Discord ID */
 export type Snowflake = string;
 
 export interface BotInfo {
-  /**
-   * The id of the bot
-   */
+  /** The id of the bot */
   id: Snowflake;
-  /**
-   * The username of the bot
-   */
+  /** The username of the bot */
   username: string;
-  /**
-   * The discriminator of the bot
-   */
+  /** The discriminator of the bot */
   discriminator: string;
-  /**
-   * The avatar hash of the bot's avatar
-   */
+  /** The avatar hash of the bot's avatar */
   avatar?: string;
-  /**
-   * The cdn hash of the bot's avatar if the bot has none
-   */
+  /** The cdn hash of the bot's avatar if the bot has none */
   defAvatar: string;
-  /**
-   * The URL for the banner image
-   */
+  /** The URL for the banner image */
   bannerUrl?: string;
   /**
    * The library of the bot
+   *
    * @deprecated
    */
   lib: string;
-  /**
-   * The prefix of the bot
-   */
+  /** The prefix of the bot */
   prefix: string;
-  /**
-   * The short description of the bot
-   */
+  /** The short description of the bot */
   shortdesc: string;
-  /**
-   * The long description of the bot. Can contain HTML and/or Markdown
-   */
+  /** The long description of the bot. Can contain HTML and/or Markdown */
   longdesc: string;
-  /**
-   * The tags of the bot
-   */
+  /** The tags of the bot */
   tags: string[];
-  /**
-   * The website url of the bot
-   */
+  /** The website url of the bot */
   website?: string;
-  /**
-   * The support server invite code of the bot
-   */
+  /** The support server invite code of the bot */
   support?: string;
-  /**
-   * The link to the github repo of the bot
-   */
+  /** The link to the github repo of the bot */
   github?: string;
-  /**
-   * The owners of the bot. First one in the array is the main owner
-   */
+  /** The owners of the bot. First one in the array is the main owner */
   owners: Snowflake[];
-  /**
-   * The guilds featured on the bot page
-   */
+  /** The guilds featured on the bot page */
   guilds: Snowflake[];
-  /**
-   * The custom bot invite url of the bot
-   */
+  /** The custom bot invite url of the bot */
   invite?: string;
-  /**
-   * The date when the bot was approved (in ISO 8601)
-   */
+  /** The date when the bot was approved (in ISO 8601) */
   date: string;
-  /**
-   * The certified status of the bot
-   */
+  /** The certified status of the bot */
   certifiedBot: boolean;
-  /**
-   * 	The vanity url of the bot
-   */
+  /** The vanity url of the bot */
   vanity?: string;
-  /**
-   * The amount of upvotes the bot has
-   */
+  /** The amount of upvotes the bot has */
   points: number;
-  /**
-   * The amount of upvotes the bot has this month
-   */
+  /** The amount of upvotes the bot has this month */
   monthlyPoints: number;
-  /**
-   * The guild id for the donatebot setup
-   */
+  /** The guild id for the donatebot setup */
   donatebotguildid: Snowflake;
 }
 
 export interface BotStats {
-  /**
-   * The amount of servers the bot is in
-   */
+  /** The amount of servers the bot is in */
   serverCount?: number;
   /**
-   * The amount of servers the bot is in per shard. Always present but can be empty. (Only when receiving stats)
+   * The amount of servers the bot is in per shard. Always present but can be
+   * empty. (Only when receiving stats)
    */
   shards?: number[];
-  /**
-   * The shard ID to post as (only when posting)
-   */
+  /** The shard ID to post as (only when posting) */
   shardId?: number;
-  /**
-   * The amount of shards a bot has
-   */
+  /** The amount of shards a bot has */
   shardCount?: number;
 }
 
 export interface UserInfo {
-  /**
-   * The id of the user
-   */
+  /** The id of the user */
   id: Snowflake;
-  /**
-   * The username of the user
-   */
+  /** The username of the user */
   username: string;
-  /**
-   * The discriminator of the user
-   */
+  /** The discriminator of the user */
   discriminator: string;
-  /**
-   * The avatar hash of the user's avatar
-   */
+  /** The avatar hash of the user's avatar */
   avatar?: string;
-  /**
-   * The cdn hash of the user's avatar if the user has none
-   */
+  /** The cdn hash of the user's avatar if the user has none */
   defAvatar: string;
-  /**
-   * The bio of the user
-   */
+  /** The bio of the user */
   bio?: string;
-  /**
-   * The banner image url of the user
-   */
+  /** The banner image url of the user */
   banner?: string;
-  /**
-   * The social usernames of the user
-   */
+  /** The social usernames of the user */
   social: {
-    /**
-     * The youtube channel id of the user
-     */
+    /** The youtube channel id of the user */
     youtube?: string;
-    /**
-     * The reddit username of the user
-     */
+    /** The reddit username of the user */
     reddit?: string;
-    /**
-     * The twitter username of the user
-     */
+    /** The twitter username of the user */
     twitter?: string;
-    /**
-     * The instagram username of the user
-     */
+    /** The instagram username of the user */
     instagram?: string;
-    /**
-     * The github username of the user
-     */
+    /** The github username of the user */
     github?: string;
   };
-  /**
-   * The custom hex color of the user
-   */
+  /** The custom hex color of the user */
   color: string;
-  /**
-   * The supporter status of the user
-   */
+  /** The supporter status of the user */
   supporter: boolean;
-  /**
-   * The certified status of the user
-   */
+  /** The certified status of the user */
   certifiedDev: boolean;
-  /**
-   * The mod status of the user
-   */
+  /** The mod status of the user */
   mod: boolean;
-  /**
-   * The website moderator status of the user
-   */
+  /** The website moderator status of the user */
   webMod: boolean;
-  /**
-   * The admin status of the user
-   */
+  /** The admin status of the user */
   admin: boolean;
 }
 
 export interface BotsQuery {
-  /**
-   * The amount of bots to return. Max. 500
-   */
+  /** The amount of bots to return. Max. 500 */
   limit?: number;
-  /**
-   * Amount of bots to skip
-   */
+  /** Amount of bots to skip */
   offset?: number;
-  /**
-   * A search string in the format of "field: value field2: value2"
-   */
+  /** A search string in the format of "field: value field2: value2" */
   search?:
     | {
         [key in keyof BotInfo]: string;
       }
     | string;
-  /**
-   * The field to sort by. Prefix with "-"" to reverse the order
-   */
+  /** The field to sort by. Prefix with "-"" to reverse the order */
   sort?: string;
-  /**
-   * A list of fields to show.
-   */
+  /** A list of fields to show. */
   fields?: string[] | string;
 }
 
 export interface BotsResponse {
-  /**
-   * The matching bots
-   */
+  /** The matching bots */
   results: BotInfo[];
-  /**
-   * The limit used
-   */
+  /** The limit used */
   limit: number;
-  /**
-   * The offset used
-   */
+  /** The offset used */
   offset: number;
-  /**
-   * The length of the results array
-   */
+  /** The length of the results array */
   count: number;
-  /**
-   * The total number of bots matching your search
-   */
+  /** The total number of bots matching your search */
   total: number;
 }
 
 export interface ShortUser {
-  /**
-   * User's ID
-   */
+  /** User's ID */
   id: Snowflake;
-  /**
-   * User's username
-   */
+  /** User's username */
   username: string;
-  /**
-   * User's discriminator
-   */
+  /** User's discriminator */
   discriminator: string;
-  /**
-   * User's avatar hash
-   */
+  /** User's avatar hash */
   avatar: string;
 }
 
 export interface WebhookPayload {
-  /**
-   * If webhook is a bot: ID of the bot that received a vote
-   */
+  /** If webhook is a bot: ID of the bot that received a vote */
   bot?: Snowflake;
-  /**
-   * If webhook is a server: ID of the server that received a vote
-   */
+  /** If webhook is a server: ID of the server that received a vote */
   guild?: Snowflake;
-  /**
-   * ID of the user who voted
-   */
+  /** ID of the user who voted */
   user: Snowflake;
   /**
-   * The type of the vote (should always be "upvote" except when using the test button it's "test")
+   * The type of the vote (should always be "upvote" except when using the test
+   * button it's "test")
    */
   type: string;
   /**
-   * Whether the weekend multiplier is in effect, meaning users votes count as two
+   * Whether the weekend multiplier is in effect, meaning users votes count as
+   * two
    */
   isWeekend?: boolean;
-  /**
-   * Query parameters in vote page in a key to value object
-   */
+  /** Query parameters in vote page in a key to value object */
   query:
     | {
         [key: string]: string;
