9a4778154ac7dd6609e2c4197e481cf7f647b1b4
null8626 <vierofernando9@gmail.com>
2025-03-10 10:39:56 +0700

    style: apply to js files too

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   ignorePatterns: ["node_modules/*", "docs/*", "dist/*"],
   extends: "@top-gg/eslint-config",
   parserOptions: {
-    project: "./tsconfig.json",
+    project: "./tsconfig.json"
   }
 };
