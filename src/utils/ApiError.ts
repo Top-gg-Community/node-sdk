f5fdf84bd277c753491fed6e0db9885bec076e4d
null <60427892+null8626@users.noreply.github.com>
2026-03-16 17:38:09 +0700

    feat: add more error tips

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,6 +1,10 @@
 const tips = {
-  401: "You need a token for this endpoint",
-  403: "You don't have access to this endpoint"
+  400: "Attempted to send an invalid request to the API",
+  401: "Invalid Top.gg API token",
+  403: "You don't have access to this endpoint",
+  404: "Such query does not exist",
+  429: "The client is blocked by the API. Please try again in a few moments",
+  500: "Received an unexpected error from Top.gg's end"
 };
 
 /** API Error */
