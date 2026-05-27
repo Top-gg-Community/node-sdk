41e4c5e5a83f5ca84929becc2dcd84832a708d2c
xNickyDev <xnickydev@gmail.com>
2026-05-27 17:09:25 +0200

    Added category for posting announcements

--- src/typings.ts
+++ src/typings.ts
@@ -19,6 +19,9 @@ export type Platform = "discord" | "roblox";
 /** A project's type */
 export type ProjectType = "bot" | "server" | "game";
 
+/** An announcement's type */
+export type AnnouncementType = "announcement" | "event" | "new_feature";
+
 /** A locale for a project's payload */
 export type Locale =
   | "en"
